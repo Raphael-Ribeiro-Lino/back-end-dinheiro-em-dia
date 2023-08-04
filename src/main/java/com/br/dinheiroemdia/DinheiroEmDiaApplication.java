3f76bed55cdd3f5990adef63e68e744b326e3a21
Raphael-Ribeiro-Lino <raphar.lino@gmail.com>
2023-08-03 21:20:00 -0300

    Adicionando projeto

---
+++ src/main/java/com/br/dinheiroemdia/DinheiroEmDiaApplication.java
@@ -0,0 +1,13 @@
+package com.br.dinheiroemdia;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DinheiroEmDiaApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DinheiroEmDiaApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/br/dinheiroemdia/DinheiroEmDiaApplicationTests.java
@@ -0,0 +1,13 @@
+package com.br.dinheiroemdia;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DinheiroEmDiaApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
