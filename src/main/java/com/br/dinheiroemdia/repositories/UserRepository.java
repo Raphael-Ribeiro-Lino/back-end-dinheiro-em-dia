50b032463f97978f98c6ebdef4060032437cf3eb
Raphael-Ribeiro-Lino <raphar.lino@gmail.com>
2023-08-07 16:02:49 -0300

    Adicionando modelMapperConfig

--- src/main/java/com/br/dinheiroemdia/repositories/UserRepository.java
+++ src/main/java/com/br/dinheiroemdia/repositories/UserRepository.java
@@ -3,12 +3,10 @@ package com.br.dinheiroemdia.repositories;
 import java.util.Optional;
 
 import org.springframework.data.jpa.repository.JpaRepository;
-import org.springframework.stereotype.Repository;
 
 import com.br.dinheiroemdia.entities.UserEntity;
 import com.br.dinheiroemdia.enums.ProfileEnum;
 
-@Repository
 public interface UserRepository extends JpaRepository<UserEntity, Long>{
 
 	Optional<UserEntity> findByProfile(ProfileEnum admin);
