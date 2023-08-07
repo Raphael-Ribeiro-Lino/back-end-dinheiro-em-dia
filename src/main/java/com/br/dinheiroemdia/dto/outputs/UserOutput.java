b071e68d9265b92de259a7ce928a703fa57e7181
Raphael-Ribeiro-Lino <raphar.lino@gmail.com>
2023-08-07 15:49:30 -0300

    Adicionando userOutput

---
+++ src/main/java/com/br/dinheiroemdia/dto/outputs/UserOutput.java
@@ -0,0 +1,15 @@
+package com.br.dinheiroemdia.dto.outputs;
+
+import lombok.Getter;
+import lombok.Setter;
+
+@Getter
+@Setter
+public class UserOutput {
+
+	private Long id;
+	
+	private String name;
+	
+	private String email; 
+}
