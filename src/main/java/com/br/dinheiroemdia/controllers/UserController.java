c6d23748362bbc786018935f07276469dd6ac73f
Raphael-Ribeiro-Lino <raphar.lino@gmail.com>
2023-08-07 21:05:59 -0300

    Adicionando userController

---
+++ src/main/java/com/br/dinheiroemdia/controllers/UserController.java
@@ -0,0 +1,12 @@
+package com.br.dinheiroemdia.controllers;
+
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import com.br.dinheiroemdia.configs.ControllerConfig;
+
+@RestController
+@RequestMapping(ControllerConfig.PRE_URL + "/user")
+public class UserController {
+
+}
