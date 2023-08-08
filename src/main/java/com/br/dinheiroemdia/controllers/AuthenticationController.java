c500757df5cbb1e980f68bfda85dd709dbcbf435
Raphael-Ribeiro-Lino <raphar.lino@gmail.com>
2023-08-08 16:21:04 -0300

    Adicionando CrossOrigin aos controllers

--- src/main/java/com/br/dinheiroemdia/controllers/AuthenticationController.java
+++ src/main/java/com/br/dinheiroemdia/controllers/AuthenticationController.java
@@ -5,6 +5,7 @@ import org.springframework.security.authentication.AuthenticationManager;
 import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
 import org.springframework.security.core.Authentication;
 import org.springframework.security.core.AuthenticationException;
+import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -20,6 +21,7 @@ import jakarta.validation.Valid;
 
 @RestController
 @RequestMapping(ControllerConfig.PRE_URL + "/auth")
+@CrossOrigin(origins = {"http://localhost", "http://localhost:4200" })
 public class AuthenticationController {
 
 	@Autowired
--- src/main/java/com/br/dinheiroemdia/controllers/UserController.java
+++ src/main/java/com/br/dinheiroemdia/controllers/UserController.java
@@ -2,6 +2,7 @@ package com.br.dinheiroemdia.controllers;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
+import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -19,6 +20,7 @@ import jakarta.validation.Valid;
 
 @RestController
 @RequestMapping(ControllerConfig.PRE_URL + "/user")
+@CrossOrigin(origins = {"http://localhost", "http://localhost:4200" })
 public class UserController {
 
 	@Autowired
