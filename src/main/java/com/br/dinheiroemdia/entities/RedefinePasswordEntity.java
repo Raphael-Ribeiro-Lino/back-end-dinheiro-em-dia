773d8143b24e6c21cd2d11accc641e5b8b9b6f99
Raphael-Ribeiro-Lino <raphar.lino@gmail.com>
2023-08-11 12:54:11 -0300

    Adicionando redefinir senha entity

---
+++ src/main/java/com/br/dinheiroemdia/entities/RedefinePasswordEntity.java
@@ -0,0 +1,30 @@
+package com.br.dinheiroemdia.entities;
+
+import java.time.LocalDateTime;
+
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import jakarta.persistence.Id;
+import jakarta.persistence.JoinColumn;
+import jakarta.persistence.ManyToOne;
+import jakarta.persistence.Table;
+import lombok.Getter;
+import lombok.Setter;
+
+@Entity
+@Table(name = "tb_redefine_password")
+@Getter
+@Setter
+public class RedefinePasswordEntity {
+
+	@Id
+	@Column(name = "hash")
+	private String hash;
+
+	@JoinColumn(name = "user_id")
+	@ManyToOne
+	private UserEntity user;
+
+	@Column(name = "expiration_time")
+	private LocalDateTime expirationTime;
+}
