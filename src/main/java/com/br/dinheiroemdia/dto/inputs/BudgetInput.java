d11337302dfc53b7138e22d54ce94896e65d24c9
Raphael-Ribeiro-Lino <raphar.lino@gmail.com>
2023-08-29 16:31:45 -0300

    Alterando input de orcamento

--- src/main/java/com/br/dinheiroemdia/dto/inputs/BudgetInput.java
+++ src/main/java/com/br/dinheiroemdia/dto/inputs/BudgetInput.java
@@ -1,5 +1,6 @@
 package com.br.dinheiroemdia.dto.inputs;
 
+import java.math.BigDecimal;
 import java.time.YearMonth;
 
 import jakarta.validation.constraints.NotNull;
@@ -12,4 +13,7 @@ public class BudgetInput {
 
 	@NotNull(message = "O mês e ano é obrigatório")
 	private YearMonth yearMonth;
+	
+	@NotNull(message = "O orçamento planejado é obrigatório")
+	private BigDecimal plannedBudget;
 }
