8a6535400c59714b16dc7a0dc2471851d032ddb0
Raphael-Ribeiro-Lino <raphar.lino@gmail.com>
2023-09-01 17:07:36 -0300

    Adicionando testes excluir despesa

--- src/test/java/com/br/dinheiroemdia/controllers/expense/DeleteExpenseControllerTest.java
+++ src/test/java/com/br/dinheiroemdia/controllers/expense/DeleteExpenseControllerTest.java
@@ -1,8 +1,9 @@
-package com.br.dinheiroemdia.controllers.income;
+package com.br.dinheiroemdia.controllers.expense;
 
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.jsonPath;
 
 import java.math.BigDecimal;
+import java.time.LocalDateTime;
 import java.time.YearMonth;
 
 import org.junit.jupiter.api.BeforeEach;
@@ -16,13 +17,14 @@ import org.springframework.test.web.servlet.ResultActions;
 
 import com.br.dinheiroemdia.configs.ControllerConfig;
 import com.br.dinheiroemdia.dto.inputs.BudgetInput;
-import com.br.dinheiroemdia.dto.inputs.IncomeInput;
+import com.br.dinheiroemdia.dto.inputs.ExpenseInput;
+import com.br.dinheiroemdia.enums.ValueTypeEnum;
 import com.br.dinheiroemdia.utils.MyMvcMock;
 
 @SpringBootTest
 @DirtiesContext(classMode = ClassMode.BEFORE_EACH_TEST_METHOD)
 @AutoConfigureMockMvc
-public class DeleteIncomeControllerTest {
+public class DeleteExpenseControllerTest {
 
 	@Autowired
 	private MyMvcMock mvc;
@@ -31,40 +33,42 @@ public class DeleteIncomeControllerTest {
 	private String token;
 
 	private BudgetInput budgetInput;
-	private IncomeInput incomeInput;
+	private ExpenseInput expenseInput;
 	
 	@BeforeEach
 	void setUp() throws Exception {
-		this.uri = ControllerConfig.PRE_URL + "/incomes/1";
+		this.uri = ControllerConfig.PRE_URL + "/expenses/1";
 		this.token = mvc.returnTokenAdm().getToken();
 
 		this.budgetInput = new BudgetInput();
 		this.budgetInput.setYearMonth(YearMonth.of(2023, 2));
 		this.budgetInput.setPlannedBudget(BigDecimal.valueOf(3000.00));
 
-		this.incomeInput = new IncomeInput();
-		this.incomeInput.setName("Salário");
-		this.incomeInput.setAmount(BigDecimal.valueOf(3000.00));
-		this.incomeInput.setBudgetId(1l);
+		this.expenseInput = new ExpenseInput();
+		this.expenseInput.setName("Aluguel");
+		this.expenseInput.setAmount(BigDecimal.valueOf(2000.00));
+		this.expenseInput.setDate(LocalDateTime.now());
+		this.expenseInput.setType(ValueTypeEnum.Fixo);
+		this.expenseInput.setBudgetId(1l);
 
 		mvc.createWithToken(ControllerConfig.PRE_URL + "/budgets", token, budgetInput);
-		mvc.createWithToken(ControllerConfig.PRE_URL + "/incomes", token, incomeInput);
+		mvc.createWithToken(ControllerConfig.PRE_URL + "/expenses", token, expenseInput);
 	}
 	
 	@Test
-	void quando_deletarReceitaSemToken_Entao_RetornaErroAcessoNegado() throws Exception {
+	void quando_deletarDespesaSemToken_Entao_RetornaErroAcessoNegado() throws Exception {
 		ResultActions result = mvc.deleteWithUnauthorized(uri);
 		result.andExpect(jsonPath("$.[?(@.message == 'Acesso Negado!')]").exists());
 	}
 	
 	@Test
-	void quando_deletarReceitaTokenInvalido_Entao_RetornaErroTokenInvalido() throws Exception {
+	void quando_deletarDespesaTokenInvalido_Entao_RetornaErroTokenInvalido() throws Exception {
 		ResultActions result = mvc.deleteWithUnauthorized(uri, token+1);
 		result.andExpect(jsonPath("$.[?(@.message == 'Token inválido!')]").exists());
 	}
 	
 	@Test
-	void quando_deletarReceita_Entao_RetornaSemConteudo() throws Exception {
+	void quando_deletarDespesa_Entao_RetornaSemConteudo() throws Exception {
 		mvc.deleteWithToken(uri, token);
 	}
 }
