9654d16d29e4f7dadcb2e0f54e7f1696342cdb54
Raphael-Ribeiro-Lino <raphar.lino@gmail.com>
2023-09-04 19:59:33 -0300

    Adicionando testes busca categoria de receita por id

--- src/test/java/com/br/dinheiroemdia/controllers/incomecategory/FindByIdIncomeCategoryControllerTest.java
+++ src/test/java/com/br/dinheiroemdia/controllers/incomecategory/FindByIdIncomeCategoryControllerTest.java
@@ -1,4 +1,4 @@
-package com.br.dinheiroemdia.controllers.income;
+package com.br.dinheiroemdia.controllers.incomecategory;
 
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.jsonPath;
 
@@ -16,13 +16,14 @@ import org.springframework.test.web.servlet.ResultActions;
 
 import com.br.dinheiroemdia.configs.ControllerConfig;
 import com.br.dinheiroemdia.dto.inputs.BudgetInput;
+import com.br.dinheiroemdia.dto.inputs.IncomeCategoryInput;
 import com.br.dinheiroemdia.dto.inputs.IncomeInput;
 import com.br.dinheiroemdia.utils.MyMvcMock;
 
 @SpringBootTest
 @DirtiesContext(classMode = ClassMode.BEFORE_EACH_TEST_METHOD)
 @AutoConfigureMockMvc
-public class FindByIdIncomeControllerTest {
+public class FindByIdIncomeCategoryControllerTest {
 
 	@Autowired
 	private MyMvcMock mvc;
@@ -32,10 +33,11 @@ public class FindByIdIncomeControllerTest {
 
 	private BudgetInput budgetInput;
 	private IncomeInput incomeInput;
+	private IncomeCategoryInput incomeCategoryInput;
 	
 	@BeforeEach
 	void setUp() throws Exception {
-		this.uri = ControllerConfig.PRE_URL + "/incomes/1";
+		this.uri = ControllerConfig.PRE_URL + "/income-categories/1";
 		this.token = mvc.returnTokenAdm().getToken();
 
 		this.budgetInput = new BudgetInput();
@@ -46,28 +48,35 @@ public class FindByIdIncomeControllerTest {
 		this.incomeInput.setName("Salário");
 		this.incomeInput.setAmount(BigDecimal.valueOf(3000.00));
 		this.incomeInput.setBudgetId(1l);
-
+		
+		this.incomeCategoryInput = new IncomeCategoryInput();
+		this.incomeCategoryInput.setName("Alugueis");
+		this.incomeCategoryInput.setIncomeId(1l);
+		
 		mvc.createWithToken(ControllerConfig.PRE_URL + "/budgets", token, budgetInput);
 		mvc.createWithToken(ControllerConfig.PRE_URL + "/incomes", token, incomeInput);
+		mvc.createWithToken(ControllerConfig.PRE_URL + "/income-categories", token, incomeCategoryInput); 
 	}
 	
 	@Test
-	void quando_buscarReceitaPorIdSemToken_Entao_RetornaErroAcessoNegado() throws Exception {
+	void quando_buscarCategoriaReceitaPorIdSemToken_Entao_RetornaErroAcessoNegado() throws Exception {
 		ResultActions result = mvc.findWithUnauthorized(uri);
 		result.andExpect(jsonPath("$.[?(@.message == 'Acesso Negado!')]").exists());
 	}
 	
 	@Test
-	void quando_buscarReceitaPorIdTokenInvalido_Entao_RetornaErroTokenInvalido() throws Exception {
+	void quando_buscarCategoriaReceitaPorIdTokenInvalido_Entao_RetornaErroTokenInvalido() throws Exception {
 		ResultActions result = mvc.findWithUnauthorized(uri, token+1);
 		result.andExpect(jsonPath("$.[?(@.message == 'Token inválido!')]").exists());
 	}
 	
 	@Test
-	void quando_buscarReceitaPorId_Entao_RetornaOk() throws Exception {
+	void quando_buscarCategoriaReceitaPorId_Entao_RetornaOk() throws Exception {
 		ResultActions result = mvc.findWithToken(uri, token);
 		result.andExpect(jsonPath("id").exists());
-		result.andExpect(jsonPath("name").value(incomeInput.getName()));
-		result.andExpect(jsonPath("amount").value(incomeInput.getAmount()));
+		result.andExpect(jsonPath("name").value(incomeCategoryInput.getName()));
+		result.andExpect(jsonPath("income.id").value(1l));
+		result.andExpect(jsonPath("income.name").value(incomeInput.getName()));
+		result.andExpect(jsonPath("income.amount").value(incomeInput.getAmount()));
 	}
 }
